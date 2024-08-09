5286be14c0cccbded35ed7e0c62b76bed962377a
Gabriel Charleboix <gcharleboix@gmail.com>
2024-08-08 22:33:07 -0500

    Initial render code

--- build.zig
+++ build.zig
@@ -26,6 +26,8 @@ pub fn build(b: *std.Build) void {
     const zigwin32_dep = b.dependency("zigwin32", .{});
 
     exe.root_module.addImport("wgpu", wgpu_native_dep.module("wgpu"));
+
+    exe.mingw_unicode_entry_point = true;
     exe.root_module.addImport("zigwin32", zigwin32_dep.module("zigwin32"));
 
     // This declares intent for the executable to be installed into the
@@ -55,18 +57,4 @@ pub fn build(b: *std.Build) void {
     // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,98 @@
 const std = @import("std");
+const win32 = @import("zigwin32");
+const Renderer = @import("renderer.zig");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+// String literals in Zig are UTF-8 encoded by default, but the Windows API needs them to be UTF-16 encoded.
+// This function converts a UTF-8 string literal to a zero-terminated UTF-16LE string literal.
+const L = win32.zig.L;
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+const windows_and_messaging = win32.ui.windows_and_messaging;
 
-    try bw.flush(); // don't forget to flush!
+const WINAPI = std.os.windows.WINAPI;
+const FALSE: c_int = 0;
+
+pub export fn wWinMain(
+    hInstance: win32.foundation.HINSTANCE,
+    _: ?win32.foundation.HINSTANCE,
+    _: [*:0]u16,
+    nCmdShow: windows_and_messaging.SHOW_WINDOW_CMD
+) callconv(WINAPI) c_int {
+    const class_name = L("wgpu-native-zig windows example");
+    const class = windows_and_messaging.WNDCLASS {
+        .style = .{}, // use style defaults for now
+        .lpfnWndProc = WindowProc,
+        .cbClsExtra = 0,
+        .cbWndExtra = 0,
+        .hInstance = hInstance,
+        .hIcon = null,
+        .hCursor = null,
+        .hbrBackground = null,
+        .lpszMenuName = null,
+        .lpszClassName = class_name,
+    };
+
+    if (windows_and_messaging.RegisterClass(&class) == 0) {
+        return FALSE;
+    }
+
+    const hwnd = windows_and_messaging.CreateWindowEx(
+        .{}, // Style again, use defaults (WINDOW_EX_STYLE).
+        class_name, // ClassName
+        L("wgpu-native-zig windows example"), // window name,
+        windows_and_messaging.WS_OVERLAPPEDWINDOW, // More style flags (WINDOW_STYLE)
+        windows_and_messaging.CW_USEDEFAULT, // Initial window top-left position X (i32)
+        windows_and_messaging.CW_USEDEFAULT, // Initial window top-left position Y (i32)
+        640, // Initial window width (i32)
+        480, // Initial window height (i32)
+        null, // Parent window handle (HWND)
+        null, // Menu handle (HMENU)
+        hInstance,
+
+        // Additional data (*anyopaque, also known as LPVOID)
+        // When we recieve a WM_CREATE message, the lParam will point to a CREATESTRUCT;
+        // the data we pass in here will show up in the lpCreateParam member of the CREATESTRUCT.
+        null,
+    ) orelse return FALSE;
+
+    _ = windows_and_messaging.ShowWindow(hwnd, nCmdShow);
+
+    var renderer = &(try Renderer.create(640, 480, hInstance, hwnd));
+
+    var msg: windows_and_messaging.MSG = undefined;
+    var should_quit = false;
+    while (!should_quit) {
+        if (windows_and_messaging.PeekMessage(&msg, hwnd, 0, 0, windows_and_messaging.PM_REMOVE) != 0) {
+            _ = windows_and_messaging.TranslateMessage(&msg);
+            _ = windows_and_messaging.DispatchMessage(&msg);
+
+            if (msg.message == windows_and_messaging.WM_QUIT) {
+                should_quit = true;
+            }
+        } else {
+            renderer.render();
+        }
+    }
+
+    renderer.release();
+    windows_and_messaging.DestroyWindow(hwnd);
+    windows_and_messaging.UnregisterClass(class_name, hInstance);
+
+    return FALSE;
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn WindowProc(
+    hwnd: win32.foundation.HWND,
+    uMsg: u32,
+    wParam: win32.foundation.WPARAM,
+    lParam: win32.foundation.LPARAM
+) callconv(WINAPI) win32.foundation.LRESULT {
+    switch (uMsg) {
+        windows_and_messaging.WM_DESTROY => {
+            windows_and_messaging.PostQuitMessage(0);
+            return 0;
+        },
+        else => {},
+    }
+    return windows_and_messaging.DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
